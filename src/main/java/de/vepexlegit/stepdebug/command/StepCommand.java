573313e213347ba11936244530b45eaa34863b0a
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-01 23:25:19 +0400

    Create StepCommand.java

---
+++ src/main/java/de/vepexlegit/stepdebug/command/StepCommand.java
@@ -0,0 +1,60 @@
+package de.vepexlegit.stepdebug.command;
+
+import de.vepexlegit.stepdebug.Step;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import java.util.Collections;
+import java.util.List;
+
+public class StepCommand implements ICommand {
+    @Override
+    public String getCommandName() {
+        return "step";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return null;
+    }
+
+    @Override
+    public List<String> getCommandAliases() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        if (args.length != 1 || (!args[0].equalsIgnoreCase("off") && !args[0].equalsIgnoreCase("on"))) {
+            sender.addChatMessage(new ChatComponentText("Invalid Arguments."));
+            return;
+        }
+
+        Step.INSTANCE.setEnabled(args[0].equalsIgnoreCase("on"));
+
+        sender.addChatMessage(new ChatComponentText(String.format("Step has been %s.",
+                Step.INSTANCE.isEnabled() ? "enabled" : "disabled")));
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(ICommand o) {
+        return 0;
+    }
+}
