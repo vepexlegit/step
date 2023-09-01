c1052f70420484aadb7e67df2ef87b8402ced1e7
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-01 23:23:04 +0400

    Create Step.java

---
+++ src/main/java/de/vepexlegit/stepdebug/Step.java
@@ -0,0 +1,15 @@
+package de.vepexlegit.stepdebug;
+
+public enum Step {
+    INSTANCE;
+
+    private boolean enabled = true;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
